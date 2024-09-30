b8e94cb0b08e7a9c362a0b6d16fb4d2081ca86b4
Fran <franrego333@gmail.com>
2024-09-30 10:34:36 +0200

    Ejercicio 31 completado

--- src/main/java/com/campusdual/classroom/Exercise31.java
+++ src/main/java/com/campusdual/classroom/Exercise31.java
@@ -1,8 +1,28 @@
 package com.campusdual.classroom;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Exercise31 {
 
     public static void main(String[] args) {
+        // Ruta al archivo "lorem.txt"
+        String filePath = "src/main/resources/lorem.txt";
 
+        try {
+            // Leer todas las líneas del archivo
+            List<String> lines = Files.readAllLines(Paths.get(filePath));
+
+            // Imprimir las líneas en la consola
+            for (String line : lines) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            // Manejo de la excepción si ocurre algún error al leer el archivo
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
     }
 }
